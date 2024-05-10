fa48c2294280284adb8168630cbe094227191f35
Jin Shian <cjinshian77@usp.br>
2024-05-10 10:25:47 -0300

    feat:	add solution to leetcode problem 454

---
+++ hash map/LC454 4Sum II.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+        int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4){
+            
+            unordered_map<int, int> m;
+            
+            for(int & i : nums1){
+                for(int & j : nums2){
+                    m[(i + j)]++;
+                }
+            }
+            
+            int count = 0;
+            
+            for(int & i : nums3){
+                for(int & j : nums4){
+                    count += m[-(i + j)];
+                }
+            }
+            
+            return count;
+        }
+};
