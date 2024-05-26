3a471a8a6f81e570ca0b6785ae1107d769e43a4b
Jin Shian <cjinshian77@usp.br>
2024-05-26 08:47:09 -0300

    feat:	add solution to leetcode problem 387

---
+++ algorithms/string/LC387 First Unique Character in a String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        int firstUniqChar(string s) {
+           
+            vector<int> count(27, 0);
+           
+            for(int i = 0; i < s.size(); ++i){
+                count[s[i] - 'a']++;
+            }
+           
+            for(int i = 0; i < s.size(); ++i){
+                if(count[s[i] - 'a'] == 1){
+                    return i;;
+                }
+            }
+           
+            return -1;
+        }
+};
