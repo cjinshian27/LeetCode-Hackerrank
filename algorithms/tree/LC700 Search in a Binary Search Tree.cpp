b99c5c8e3d00546a5b553ddb2d25315ec9a4f48e
Jin Shian <cjinshian77@usp.br>
2024-05-30 08:25:57 -0300

    feat:	add solution to leetcode problem 700

---
+++ algorithms/tree/LC700 Search in a Binary Search Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        TreeNode* searchBST(TreeNode* root, int val) {
+        
+            if(!root){
+                return nullptr;
+            }
+            if(val < root->val){
+                return searchBST(root->left, val);
+            }
+            else if(val > root->val){
+                return searchBST(root->right, val);
+            }
+            else{
+                return root;
+            }
+        }
+};
