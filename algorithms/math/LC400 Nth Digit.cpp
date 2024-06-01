3a3d7ced9bce9c0829809884a74741946c2b84e1
Jin Shian <cjinshian77@usp.br>
2024-06-01 20:15:41 -0300

    feat:	add solution to leetcode problem 400

---
+++ algorithms/math/LC400 Nth Digit.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        int findNthDigit(int n){
+
+            long digit = 9;
+            int first = 1;
+            int length = 1;
+            
+            while(n > digit * length){
+                
+                n = n - digit*length;
+                length++;
+                first = first * 10;
+                digit = digit * 10;
+            }
+            
+            first = first + (n - 1)/length;
+            string str = to_string(first);
+            
+            return str[(n - 1)%length] - '0';
+        }
+};
